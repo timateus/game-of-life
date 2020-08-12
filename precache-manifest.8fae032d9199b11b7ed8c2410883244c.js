f1f54ef9c2b58bca44231e7c75bd9551ab947f24
Tim Nosov <timnosov@gmail.com>
2020-08-12 14:53:07 +0300

    Updates

---
+++ precache-manifest.8fae032d9199b11b7ed8c2410883244c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf42fa131c35bda1ea0c1ddebb49f03d",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "f501af2db7434784c18e",
+    "url": "/game-of-life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6922b5da8ffcbd080c21",
+    "url": "/game-of-life/static/js/2.fb565563.chunk.js"
+  },
+  {
+    "revision": "3df497401d324ee6ed76d4d98715bc5f",
+    "url": "/game-of-life/static/js/2.fb565563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f501af2db7434784c18e",
+    "url": "/game-of-life/static/js/main.f5277db7.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
